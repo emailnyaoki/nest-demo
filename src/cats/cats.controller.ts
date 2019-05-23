c88b61108a8e0d4580b7ec18a25d00598f54e9f4
onwl007 <onwl007@126.com>
2019-05-23 18:03:08 +0800

    异常过滤器

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,20 +1,27 @@
-import { Controller, Get, Req, Post, HttpCode, Header, Param, Body } from '@nestjs/common';
+import { Controller, Get, Req, Post, HttpCode, Header, Param, Body, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Request } from 'express';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
+import { HttpExceptionFilter } from 'src/common/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  // 传递是实例，如果可以最好传递类而不是实例，可以删少内存使用量
+  @UseFilters(new HttpExceptionFilter())
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
 
   @Get()
   async findAll(): Promise<Cat[]> {
+    throw new HttpException({
+      status: HttpStatus.FORBIDDEN,
+      error: 'This is a custom message',
+    }, 403);
     return this.catsService.findAll();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './common/logger.middleware';
+import { HttpExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 全局中间件
   app.use(logger);
+  // 全局范围的异常过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
