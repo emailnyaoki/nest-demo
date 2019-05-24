e22ae1b3a431245eb23adcc1a540732eeda03881
onwl007 <onwl007@126.com>
2019-05-24 16:19:27 +0800

    守卫者

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Req, Post, HttpCode, Header, Param, Body, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpException, HttpStatus, UseFilters, UseGuards, SetMetadata } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Request } from 'express';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
 import { HttpExceptionFilter } from 'src/common/http-exception.filter';
+import { RolesGuard } from 'src/roles.guard';
+import { Roles } from './roles.decorator';
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  @Roles('admin')
   // 传递是实例，如果可以最好传递类而不是实例，可以删少内存使用量
   @UseFilters(new HttpExceptionFilter())
   async create(@Body() createCatDto: CreateCatDto) {
